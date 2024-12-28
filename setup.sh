b66053895be69db4a84d15612bd5fd1894ce2ac2
Joshua Daniel <joshua-daniel@mailfence.com>
2024-12-28 14:49:06 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,3 @@ APT::Install-Recommends "0";
 APT::Install-Suggests "0";
 " >> /etc/apt/apt.conf
 sudo apt update
-sudo apt install i3 xorg htop neofetch firefox-esr feh dmenu; sudo apt remove i3lock
-mv wall.jpg ~
-mkdir -p ~/.config/i3/
-mv config ~/.config/i3/
